45673ad0207591f97bb9fa7fdbf187680c0748a1
jakub.wylezek <jakub.wylezek@wp.pl>
2019-12-11 07:46:03 +0100

    Initial commit

---
+++ src/main/java/pl/wylezek/petclinic/petservice/PetServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.wylezek.petclinic.petservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/wylezek/petclinic/petservice/PetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wylezek.petclinic.petservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
