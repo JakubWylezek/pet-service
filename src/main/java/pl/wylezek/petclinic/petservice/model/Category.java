4e5407bf19ad402783283defcaf3e5e4e7a5926e
jakub.wylezek <jakub.wylezek@wp.pl>
2019-12-11 08:03:09 +0100

    Created model for category

---
+++ src/main/java/pl/wylezek/petclinic/petservice/model/Category.java
@@ -0,0 +1,33 @@
+package pl.wylezek.petclinic.petservice.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    @JsonIgnore
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private Set<Category> subCategories;
+
+    @OneToMany(mappedBy = "category")
+    private Set<Pet> pets;
+}
